6dee82b21aa9d6e53b20b5776343bee9c45c6268
Prince02031 <prince003kn@gmail.com>
2026-05-23 20:12:43 +0600

    unrelated project removed from project folder

--- ProjectOdyssey - Copy/client/odyssey/app/components/visit/index.ts
+++
@@ -1,6 +0,0 @@
-export { CheckInButton } from './CheckInButton';
-export { CheckOutModal } from './CheckOutModal';
-export { ActiveVisitCard } from './ActiveVisitCard';
-export { VisitHistory } from './VisitHistory';
-export { TripProgress } from './TripProgress';
-export { VisitStats } from './VisitStats';
